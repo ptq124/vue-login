1b9ed30520feb11f239f8834d751ff8510518a78
박태규 Park Tae Kyu <ptq124@naver.com>
2022-11-18 03:33:39 +0900

    jwt token 추가

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,51 @@
-from fastapi import FastAPI
+from datetime import timedelta
+from jose import JWTError,jwt
+import uvicorn
+from fastapi import FastAPI,Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from user import create_access_token,authenticate_user, fake_users_db, ACCESS_TOKEN_EXPIRE_MINUTES,SECRET_KEY,ALGORITHM,User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post('/token')
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="사용자가 존재하지 않습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_jwt_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='인증 정보를 검증할 수 없습니다.',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_dict: dict = payload.get('user')
+        if not user_dict:
+            raise credentials_exception
+        user = User(**user_dict)
+    except JWTError:
+        raise credentials_exception
+    if user is None:
+        raise credentials_exception
+    return user
+
+if __name__ == "__main__":
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
---
+++ backend/user.py
@@ -0,0 +1,62 @@
+from datetime import timedelta, datetime
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Token:
+  access_token: str
+  token_type: str
+  username: str
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+fake_users_db = {
+    "admin": {
+        "username": "admin",
+        "full_name": "Tae Kyu",
+        "email": "admin@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+}
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
