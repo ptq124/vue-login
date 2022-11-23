dbfbab8e0c66ccaf4bbb635a07106c50b82b15a2
박태규 Park Tae Kyu <ptq124@naver.com>
2022-11-24 02:58:23 +0900

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import store from './store.js'
-
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app')
