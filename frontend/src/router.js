be21be749381adbe93a5158cf2d1e870864a9f59
박태규 Park Tae Kyu <ptq124@naver.com>
2022-11-24 02:58:12 +0900

    router 로그인 권환 설정

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,12 +3,22 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const requireAuth = () => (to,from,next)=>{
+  const token = localStorage.getItem('access_token')
+
+  if(token){
+    return next()
+  }
+  next('/')
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: () => import('./views/LoginForm.vue') },
-    { path: '/todo', component: () => import('./views/TodoList.vue') },
+    { path: '/todo', component: () => import('./views/TodoList.vue'), beforeEnter:requireAuth() },
   ],
 })
 
 export default router
+
