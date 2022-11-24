db0b0d55a28cfc6e0eac3f7d5153f309dbf2c31b
박태규 Park Tae Kyu <ptq124@naver.com>
2022-11-24 21:57:01 +0900

    validation 기능추가

--- frontend/src/views/LoginForm.vue
+++ frontend/src/views/LoginForm.vue
@@ -4,48 +4,33 @@
       <div class="row h-25 align-items-end">
         <div class="text-center fs-1">login</div>
       </div>
-      <div
-        class="row h-50 flex-column align-items-center justify-content-center"
-      >
+      <div class="row h-50 flex-column align-items-center justify-content-center">
         <div class="w-50">
           <input
             type="text"
             class="form-control"
             placeholder="Username"
-            style="
-              border-radius: 25px;
-              border: solid 0.5px #7d86a9;
-              padding: 12px 10px;
-            "
+            style="border-radius: 25px; border: solid 0.5px #7d86a9; padding: 12px 10px;"
             v-model="username"
             @keyup.enter="login()"
           />
+          <div class="text-center text-danger" v-show="msg">{{msg}}</div>
         </div>
         <div class="w-50 mt-3">
           <input
             type="password"
             class="form-control"
             placeholder="Password"
-            style="
-              border-radius: 25px;
-              border: solid 0.5px #7d86a9;
-              padding: 12px 10px;
-            "
+            style="border-radius: 25px; border: solid 0.5px #7d86a9; padding: 12px 10px;"
             v-model="password"
             @keyup.enter="login()"
           />
+          <div class="text-center text-danger" v-show="msg1">{{msg1}}</div>
         </div>
       </div>
       <div class="row h-25 justify-content-center">
         <div class="w-50">
-          <button
-            type="button"
-            class="btn btn-outline-secondary w-100"
-            style="padding: 12px 10px; border-radius: 25px"
-            @click="login"
-          >
-            Login
-          </button>
+          <button type="button" class="btn btn-outline-secondary w-100" style="padding: 12px 10px; border-radius: 25px" @click="login">Login</button>
         </div>
       </div>
     </div>
@@ -59,8 +44,14 @@ export default {
     return {
       username: '',
       password: '',
+      msg: '',
+      msg1: '',
     }
   },
+  watch: {
+    'username' : 'validatationId',
+    'password' : 'validatationPwd'
+  },  
   methods: {
     async login() {
       try {
@@ -74,6 +65,20 @@ export default {
         alert('로그인 오류')
       }
     },
+    validatationId(){
+      if(this.username.length > 5){
+        this.msg = 'id길이를 5이하로 작성해주세요'
+      }else{
+        this.msg = ''
+      }
+    },
+    validatationPwd(){
+      if(this.password.length > 5){
+        this.msg1 = '비밀번호 길이를 6이하로 작성해주세요'
+      }else{
+        this.msg1 = ''
+      }
+    }
   },
 }
 </script>
